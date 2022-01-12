dffd64e55594093ee3060a2ef101f3b78648744c
rus <kipnis.rus@gmail.com>
2022-01-11 21:42:15 -0500

    working zipcode search, closes #1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React ,{ Component } from 'react';
-import ZipCodes from './components/zipsearch'
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ZipCodes from "./components/zipsearch";
+import ReactDOM from "react-dom";
 
-class App extends Component {
-	
-  render() {
-  
+function App() {
+  const [zip, setZip] = useState(""); // empty state to use user input from handle change function
+  const [list, setList] = useState([]); // list that gets back from api (empty array)
+
   return (
-      <div>
-        <ZipCodes />
-     </div>   
-     );
-  }
+    <div>
+      <div className="container bg-dark text-center text-white justify-content-center p-4 ">
+        <h1>Zip Codes Search</h1>
+      </div>
+      <div className="container text-center">
+        <ZipCodes zip={zip} list={list} setList={setList} setZip={setZip} />
+        {/* value and pair object key=value */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/zipsearch.js
+++ src/components/zipsearch.js
@@ -1,46 +1,63 @@
-import React, {useEffect, useState} from "react"
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 // import ReactDOM, { render } from 'react-dom';
 
-function ZipCodes(props)
-    {
-        const [item, setItem] = useState([]);
-        // const [seletedZip, setSelectedZip] = useState([]);
-        useEffect(() => {
-           axios.get("http://ctp-zip-api.herokuapp.com/zip/"+props).then((response) => {
-              const data = setItem(response.data);
-                console.log(data);  
-            });
-        },[]);
-    
-        // if(!setItems)
-        // {
-        //     return <h3>not found</h3>
-        // }
-        
+function ZipCodes(props) {
+  const { zip, setZip, list, setList } = props; //passing down as child
 
+  const handleSubmit = (event) => {
+    //once user hits submit button
+    event.preventDefault();
+    axios
+      .get("http://ctp-zip-api.herokuapp.com/zip/" + zip)
+      .then((response) => {
+        setList(response.data);
+      });
+    // console.log(event.target.input);
+  };
+  const handleChange = (event) => {
+    event.preventDefault(); //prevents refresh
+    setZip(event.target.value); //input will get the value and setting setZip value
+  };
+  return (
+    <>
+      <div className="container text-center justify-content-center p-2">
+        <form onSubmit={handleSubmit}>
+          <label for="zipsearch">Zip Code:</label>
+          <input onChange={handleChange} name="zipcode" type="text"></input>
+          <button type="submit" value="Submit">
+            Submit
+          </button>
+        </form>
 
-    return (
-        <>
-        <div >
-             <label for="zipsearch">Enter Zip Code</label>
-                <input id="zip" type="number"></input>
-                <button type="submit" value="Submit">Submit</button>
-         
-        {item.map((element) => {
-                    return (
-                       <ul className="d-inline-flex list-group list-group">
-                                <li onChange="zip" className="list-group-item">Zip: {element.State}</li>
-                                <li className="list-group-item">City: {element.City}</li>
-                                <li className="list-group-item">Location: {element.Lat}, {element.Long}</li>
-                                <li className="list-group-item">Wages: {element.TotalWages}</li>
-                            </ul>
-                    );
-                })}
+        {list.map((element) => {
+          return (
+            <div class="d-flex justify-content-center ">
+              <ul className="list-group list-group border m-3">
+                <div className="text-start">
+                  <h5
+                    className="text-dark  bg-secondary text-center"
+                    style={{ height: "3rem" }}
+                  >
+                    {element.City}, {element.State}
+                  </h5>
+                  <li className="list-group">• State: {element.State}</li>
+                  <li className="list-group">
+                    • Location: ({element.Lat}, {element.Long})
+                  </li>
+                  <li className="list-group">
+                    • Population (estimated): {element.EstimatedPopulation}
+                  </li>
+                  <li className="list-group marker">
+                    • Wages: {element.TotalWages}
+                  </li>
+                </div>
+              </ul>
             </div>
-       </>
-
-    );
+          );
+        })}
+      </div>
+    </>
+  );
 }
-
 export default ZipCodes;
